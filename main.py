df52c7b75e4b7d1afc22a52dee7df4260c6f0244
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-01-05 16:59:46 -0500

    add finishing touches

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CourseSchedulePlanner import CourseSchedulePlanner
 
 print('\nThe Course Schedule Planner creates a schedule for you based on the given course data\n')
-print('Courses are inputted in the format:\n\'CourseName: List of prerequisites for the course seperated by comma and space\'')
+print('Courses are inputted in the format:\n\'CourseName: List of prerequisites for the course separated by comma and space\'')
 print(' Example-> CS150: CS101, CS121, MATH140')
 print(' Example of course with no prerequisites-> CS101\n')
 
@@ -12,21 +12,21 @@ schedulePlanner = CourseSchedulePlanner(5)
 def isEmpty(input_string):
     return input_string.isspace() or input_string == ''
 
-while (query := input('Enter a course and it\'s prerequisites (enter `q` when finished)> ')) != 'q':
+while (query := input('Enter a course and its prerequisites (enter `q` when finished)> ')) != 'q':
     if query == '':
         continue
 
     try:
-        course_and_prerequites = query.split(': ', 1)
+        course_and_prerequisites = query.split(': ', 1)
     except ValueError:
         print('Error occured split failed')
         continue
-    if ':' in course_and_prerequites[-1] or isEmpty(course_and_prerequites[0]):
+    if ':' in course_and_prerequisites[-1] or isEmpty(course_and_prerequisites[0]):
         print('invalid input, please try again')
         continue
 
     try:
-        prerequisites = course_and_prerequites[-1].split(', ')
+        prerequisites = course_and_prerequisites[-1].split(', ')
     except ValueError:
         print('Error occured split failed')
         continue
@@ -34,15 +34,15 @@ while (query := input('Enter a course and it\'s prerequisites (enter `q` when fi
         print('invalid input, please try again')
         continue
 
-    if prerequisites == course_and_prerequites:
+    if prerequisites == course_and_prerequisites:
         prerequisites = []
-        schedulePlanner.course_prereqs[course_and_prerequites[0]] = prerequisites
+        schedulePlanner.course_prereqs[course_and_prerequisites[0]] = prerequisites
     else:
-        schedulePlanner.course_prereqs[course_and_prerequites[0]] = prerequisites
+        schedulePlanner.course_prereqs[course_and_prerequisites[0]] = prerequisites
         prerequisites = set(prerequisites)
 
     #Build adjecency list
-    schedulePlanner.build_adjecency_list(prerequisites, course_and_prerequites[0])
+    schedulePlanner.build_adjecency_list(prerequisites, course_and_prerequisites[0])
 
 if not schedulePlanner.courses:
     print('\nNo courses were added\n')
